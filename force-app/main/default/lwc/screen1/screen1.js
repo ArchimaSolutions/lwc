5174eeaad42d14da3044bc6ef686caf29d4803e8
Danny <zolmore@gmail.com>
2019-06-25 12:08:08 -0400

    Fixed user name.

--- force-app/main/default/lwc/screen1/screen1.js
+++ force-app/main/default/lwc/screen1/screen1.js
@@ -1,7 +1,9 @@
 import { LightningElement } from 'lwc';
 
 import Id from '@salesforce/user/Id';
+import Name from '@salesforce/user/Name';
 
 export default class Screen1 extends LightningElement {
     userId = Id;
+    userName = Name;
 }
